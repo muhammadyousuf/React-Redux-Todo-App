360cb91e212e1319de548b1d62d88340cc40be6f
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-03-15 17:49:03 +0500

    add signup form

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import HoverIncrement from './Component/Screens/Hover-Increment/Hover-Increment.jsx';
 import ButtonIncrement from './Component/Screens/Button-Increment/Button-Increment.jsx'; 
+import Signup from './Component/Screens/Signup/Signup';
 class App extends Component {
   render() {
     return (
       <div className="App">
          <HoverIncrement name="Yousuf" />
          <ButtonIncrement />
+         <Signup />
       </div>
     );
   }

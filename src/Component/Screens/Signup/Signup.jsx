6f4d83ddb36d0493ead5d369ac2ad84b3a6d0a32
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-03-19 15:18:56 +0500

    signup design

--- src/Component/Screens/Signup/Signup.jsx
+++ src/Component/Screens/Signup/Signup.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import {
-    Form, Icon, Input, Button,
+    Form, Icon, Input, Button,Card
 } from 'antd';
-import {withRouter} from 'react-router-dom';
-import {reset} from '../../Function/function';
+import { withRouter } from 'react-router-dom';
+import { reset } from '../../Function/function';
 import Navbar from '../Navbar/Navbar';
+import './Signup.css';
 
 
 class Signup extends Component {
@@ -20,49 +21,61 @@ class Signup extends Component {
             }
         });
     }
-  
-  
+
+
     render() {
         const { getFieldDecorator } = this.props.form;
         return (
             <div >
                 <Navbar />
-                <Form onSubmit={this.handleSubmit} className="login-form">
-                <Form.Item>
-                        {getFieldDecorator('firstname', {
-                            rules: [{ required: true, message: 'Please input your first name!' }],
-                        })(
-                            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="First Name" />
-                        )}
-                    </Form.Item>
-                    <Form.Item>
-                        {getFieldDecorator('lastname', {
-                            rules: [{ required: true, message: 'Please input your last name!' }],
-                        })(
-                            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Last Name" />
-                        )}
-                    </Form.Item>
-                    <Form.Item>
-                        {getFieldDecorator('email', {
-                            rules: [{ required: true, message: 'Please input your email!' }],
-                        })(
-                            <Input  prefix={<Icon type="message" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="email" />
-                        )}
-                    </Form.Item>
-                    <Form.Item>
-                        {getFieldDecorator('password', {
-                            rules: [{ required: true, message: 'Please input your Password!' }],
-                        })(
-                            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-                        )}
-                    </Form.Item>
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit" className="login-form-button">
-                        Register
+                <div style={{
+                    width: '100',
+                    textAlign: 'center',
+                    marginTop: 100
+                }} >
+                    <Card
+                        title="User Registeration"
+                        headStyle={{ fontSize: 40, fontFamily: 'roboto' }}
+                        style={{ width: 400, display: 'inline-block' }}
+                    >
+                        <Form onSubmit={this.handleSubmit} className="login-form">
+                            <Form.Item className="Signup-input-box" >
+                                {getFieldDecorator('firstname', {
+                                    rules: [{ required: true, message: 'Please input your first name!' }],
+                                })(
+                                    <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="First Name" />
+                                )}
+                            </Form.Item>
+                            <Form.Item className="Signup-input-box" >
+                                {getFieldDecorator('lastname', {
+                                    rules: [{ required: true, message: 'Please input your last name!' }],
+                                })(
+                                    <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Last Name" />
+                                )}
+                            </Form.Item>
+                            <Form.Item className="Signup-input-box" >
+                                {getFieldDecorator('email', {
+                                    rules: [{ required: true, message: 'Please input your email!' }],
+                                })(
+                                    <Input prefix={<Icon type="fund" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="email" />
+                                )}
+                            </Form.Item>
+                            <Form.Item className="Signup-input-box" >
+                                {getFieldDecorator('password', {
+                                    rules: [{ required: true, message: 'Please input your Password!' }],
+                                })(
+                                    <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                                )}
+                            </Form.Item>
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" className="login-form-button">
+                                    Register
           </Button>
-                        <div> <a href="/">Login Here!</a></div>
-                    </Form.Item>
-                </Form>
+                                <div> <a href="/">Login Here!</a></div>
+                            </Form.Item>
+                        </Form>
+                    </Card>
+                </div>
             </div>
         );
     }

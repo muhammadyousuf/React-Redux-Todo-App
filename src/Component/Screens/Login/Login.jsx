4b605e7ee1975d3d5696ab118fd1f84ea12da200
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-03-19 14:01:09 +0500

    Login design

--- src/Component/Screens/Login/Login.jsx
+++ src/Component/Screens/Login/Login.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import {
-    Form, Icon, Input, Button, Checkbox,
+    Form, Icon, Input, Button, Checkbox, Row, Col, Card
 } from 'antd';
-
-import {withRouter} from 'react-router-dom';
-import {reset} from '../../Function/function';
+import './Login.css';
+import { withRouter } from 'react-router-dom';
+import { reset } from '../../Function/function';
 import Navbar from '../Navbar/Navbar';
 
 
@@ -16,58 +16,79 @@ class Login extends Component {
         this.props.form.validateFields((err, values) => {
             if (!err) {
                 console.log('Received values of form: ', values);
-               let obj = this.props.user.AuthReducer.user
-               
-               if(obj.email === values.email && obj.password === values.password){
-                   let user = obj.firstname+' '+obj.lastname
-                   localStorage.setItem('token','abc12345')
-                   localStorage.setItem('name',user)
-                   reset(this.props);
-                   this.props.history.push('/ADDTODO')
-               }
+                let obj = this.props.user.AuthReducer.user
+
+                if (obj.email === values.email && obj.password === values.password) {
+                    let user = obj.firstname + ' ' + obj.lastname
+                    localStorage.setItem('token', 'abc12345')
+                    localStorage.setItem('name', user)
+                    reset(this.props);
+                    this.props.history.push('/ADDTODO')
+                }
             }
         });
     }
-    
+    signup = () => {
+        console.log("abc")
+    }
     render() {
         const { getFieldDecorator } = this.props.form;
         return (
             <div >
                 <Navbar />
-                <Form onSubmit={this.handleSubmit} className="login-form">
-                    <Form.Item>
-                        {getFieldDecorator('email', {
-                            rules: [{ required: true, message: 'Please input your email!' }],
-                        })(
-                            <Input prefix={<Icon type="message" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="email" />
-                        )}
-                    </Form.Item>
-                    <Form.Item>
-                        {getFieldDecorator('password', {
-                            rules: [{ required: true, message: 'Please input your Password!' }],
-                        })(
-                            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-                        )}
-                    </Form.Item>
-                    <Form.Item>
-                        {getFieldDecorator('remember', {
-                            valuePropName: 'checked',
-                            initialValue: true,
-                        })(
-                            <Checkbox>Remember me</Checkbox>
-                        )}
-                        <div>
-                            <a className="login-form-forgot" href="/Login">Forgot password</a>
-                        </div>
-                        <div>
-                            <Button type="primary" htmlType="submit" className="login-form-button">
-                                Login
-          </Button>
-                        </div>
-                        <div> <a href="/Signup">register now!</a></div>
+                <div style={{
+                    width: '100',
+                    textAlign: 'center',
+                    marginTop:100
+                }} >
+                    <Card
+                        title="User Login"
+                       headStyle={{fontSize:40, fontFamily:'roboto'}}
+                        style={{ width: 400, display: 'inline-block' }}
+                    >
+                        <Form onSubmit={this.handleSubmit} className="login-form">
+                            <Row>
+                                <Col span={2}>
 
-                    </Form.Item>
-                </Form>
+                                    <Form.Item className="Login-input-box" >
+                                        {getFieldDecorator('email', {
+                                            rules: [{ required: true, message: 'Please input your email!' }],
+                                        })(
+                                            <Input prefix={<Icon type="fund" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="email" />
+                                        )}
+                                    </Form.Item>
+                                </Col>
+                            </Row>
+
+                            <Form.Item className="Login-input-box" >
+                                {getFieldDecorator('password', {
+                                    rules: [{ required: true, message: 'Please input your Password!' }],
+                                })(
+                                    <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                                )}
+                            </Form.Item>
+                            <Form.Item >
+                                {getFieldDecorator('remember', {
+                                    valuePropName: 'checked',
+                                    initialValue: true,
+                                })(
+                                    <Checkbox>Remember me</Checkbox>
+                                )}
+                                <div>
+                                    <a className="login-form-forgot" href="/Login">Forgot password</a>
+                                </div>
+                                <div>
+                                    <Button type="primary" htmlType="submit" className="login-form-button">
+                                        Login
+          </Button>
+                                </div>
+                                <div onClick={this.signup} className="Register-link"   >
+                                    Register now!</div>
+
+                            </Form.Item>
+                        </Form>
+                    </Card>
+                </div>
             </div>
         );
     }

ad42b7189373f54a1c54649c92e61af4ca331db6
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-03-22 14:46:21 +0500

    add todo design

--- src/Component/Screens/ADD TODO/ADDTODO.jsx
+++ src/Component/Screens/ADD TODO/ADDTODO.jsx
@@ -1,25 +1,109 @@
-import React, { Component } from 'react';
-import {withRouter} from 'react-router-dom';
-import Navbar from '../Navbar/Navbar';
-
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+import { Form, Icon, Input, Button, Card } from "antd";
+import { reset } from "../../Function/function";
 
 class ADDTODO extends Component {
-   
-   
-    componentDidMount(){
-        console.log('user', this.props)
-        
+    constructor(props) {
+        super(props);
+    
+        const value = props.value || {};
+        this.state = {
+          number: value.number || 0,
+          currency: value.currency || 'rmb',
+        };
+      }
+  componentDidMount() {
+    console.log("user", this.props);
+  }
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.props);
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+        this.props.addUser(values);
+        reset(this.props);
+        this.props.history.push("/");
+      }
+    });
+  };
+  handleNumberChange = (e) => {
+    const number = parseInt(e.target.value || 0, 10);
+    if (Number.isNaN(number)) {
+      return;
     }
-    render() {
-        return (
-            <div >
-               <Navbar />
-            </div>
-        );
+    if (!('value' in this.props)) {
+      this.setState({ number });
     }
+    this.triggerChange({ number });
+  }
+  triggerChange = (changedValue) => {
+    // Should provide an event to pass value to Form.
+    const onChange = this.props.onChange;
+    if (onChange) {
+      onChange(Object.assign({}, this.state, changedValue));
+    }
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const { size } = this.props;
+    const state = this.state;
+    return (
+      <div>
+        <Navbar />
+        <div
+          style={{
+            width: "100",
+            textAlign: "center",
+            marginTop: 100
+          }}
+        >
+          <Card
+            title="ADD TODO"
+            headStyle={{ fontSize: 40, fontFamily: "roboto" }}
+            style={{ width: 400, display: "inline-block" }}
+          >
+            <Form onSubmit={this.handleSubmit} className="login-form">
+              <Form.Item className="Signup-input-box">
+                {getFieldDecorator("todo", {
+                  rules: [{ required: true, message: "Please add todo!" }]
+                })(
+                  <Input
+                    prefix={
+                      <Icon type="form" style={{ color: "rgba(0,0,0,.25)" }} />
+                    }
+                    placeholder="TODO"
+                  />
+                )}
+              </Form.Item>
+              <Form.Item className="Signup-input-box">
+                <Input
+                  type="text"
+                  size={size}
+                  value={state.number}
+                  onChange={this.handleNumberChange}
+                  style={{ width: "100%", marginRight: "3%" }}
+                />
+              </Form.Item>
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="login-form-button"
+                >
+                  ADD TODO
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </div>
+      </div>
+    );
+  }
 }
 
-
-
-
-export default withRouter(ADDTODO) 
+const ADDTODOForm = Form.create({ name: "normal_todo" })(ADDTODO);
+export default withRouter(ADDTODOForm);
--- src/Component/Screens/TODO LIST/TODOLIST.jsx
+++ src/Component/Screens/TODO LIST/TODOLIST.jsx
@@ -29,7 +29,7 @@ const columns = [
     render: (text, record) => (
       <span>
         {/* {console.log(record.id)} */}
-        <a onClick={this.handleDelete.bind(this, record)} >Delete</a>
+        <a  >Delete</a>
       </span>
     )
   }
@@ -60,50 +60,3 @@ class TODOLIST extends React.Component {
   }
 }
 export default withRouter(TODOLIST);
-// const {
-//   Header, Content,
-// } = Layout;
-// class AllTodo extends React.Component {
-//   constructor(props) {
-//     super(props);
-//    this.delete = this.handleDelete.bind(this);
-//     const { GetTodos } = this.props;
-//     GetTodos();
-
-//   }
-//   handleDelete(todoId){
-//     const {DeleteTodo,history} = this.props
-//     DeleteTodo(todoId.id,history)
-//   }
-
-//   render() {
-//     const {todos} = this.props;
-//     return (
-//       <div >
-//       <Layout>
-//       <Header>All Todos <span className="add-todo-btn"><Link to="/addtodo">+</Link></span>
-
-//       </Header>
-//       <Content>
-//     <List
-//       bordered
-//       dataSource={todos}
-//       renderItem={(item,index) => (
-//         <List.Item key={index}>
-//         <Row>
-//           <Col span={18}>
-//           <Link to={`/edittodo/${item.id}`}>{item.title}</Link>
-//           </Col>
-//            <Col span={6}>
-//           </Col>
-//           <Col span={6}>
-//           <Button type="danger"  onClick={this.handleDelete.bind(this, item)} >Delete</Button>
-//           </Col>
-//         </Row>
-//       </List.Item>)}
-//     /></Content>
-//     </Layout>
-//       </div>
-//     );
-//   }
-// }
